01b5c99bf6414d3b8aae734a0662090326a521b0
Abdullah Altwaim <a_t1411@hotmail.com>
2019-05-29 03:00:12 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,19 +4,34 @@
 // var cardFour = "king";
 // console.log("User flipped " + cardOne);
 // alert('Hello, friends.');
-var cards = ["queen","queen","king","king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardInPlay = [];
-var cardOne = cards[0];
-cardInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if(cardInPlay.length === 2){
-    if(cardInPlay[0]===cardInPlay[1]){
-        alert("You found a match!");
-    } else{
-        alert("Sorry, try again."); 
-    }
+function checkForMatch(){
+    if (cardInPlay[0] === cardInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
 
 }
+function flipCard(cardId) {
+    if (cardInPlay.length === 2) {
+        if (cardInPlay[0] === cardInPlay[1]) {
+            alert("You found a match!");
+        } else {
+            alert("Sorry, try again.");
+        }
+    
+    }
+     
+    console.log("User flipped " + cards[cardId])
+    cardInPlay.push(cards[cardId]);
+    // checkForMatch();
+    
+    
+
+}
+flipCard(0);
+flipCard(2);
+ checkForMatch();
+
